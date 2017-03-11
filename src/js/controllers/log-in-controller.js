c2f2aef7d2848bc23469ec94ffda55dfe0016c61
Nicholas Bryant <nicholas@Nicholass-MacBook-Pro.local>
2017-03-11 17:34:00 -0500

    correct an issue with login page

--- src/js/controllers/log-in-controller.js
+++ src/js/controllers/log-in-controller.js
@@ -1,6 +1,7 @@
 (function() {
   angular.module('dopplerApp').controller('logInController', function($state, $q, UserService, StorageService) {
-
+    let users = [];
+    
     this.authenticate = function(username) {
       users = StorageService.get('all-users');
 
